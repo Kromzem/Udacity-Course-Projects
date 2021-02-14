87ae75082aee189c4bd596a25a86a656e239f0c0
kromzem <kromzem.games@gmail.com>
2021-02-14 16:32:43 +0100

    finished

--- DiceRolle/app/src/main/java/com/kromzem/diceroller/MainActivity.kt
+++ DiceRolle/app/src/main/java/com/kromzem/diceroller/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.kromzem.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.roll_button)
+        button.setOnClickListener {
+            rollDice()
+        }
+
+        diceImage = findViewById(R.id.dice_image)
+    }
+
+    private fun rollDice() {
+        val imageResource = when (Random().nextInt(6)) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(imageResource)
     }
 }
